106782bf287f3690199879258026b56cb48d8d74
Mohammed Numaan <138296610+mohammednumaan@users.noreply.github.com>
2023-08-03 13:13:07 +0530

    Updated

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 // import
-import "./style.css"
 import { DOM } from "./main/DOM";
 import { displayLocalStorage } from "./localstorage/displayStorage";
 import { getLocalStorage } from "./localstorage/localStorage";
+import "./style.css"
 
 
 // initialize
